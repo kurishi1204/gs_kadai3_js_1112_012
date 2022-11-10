ec7f18a0373e99f5b01a8c17fc44b093ec68ddc0
kurishi1204 <kurishima@hkbn.vc>
2022-11-10 09:41:41 +0900

    処理コメントなどを追加

--- js/script.js
+++ js/script.js
@@ -2,37 +2,48 @@
 
 const storage = localStorage;
 
-const table = document.querySelector('table');
-const todo = document.getElementById('todo');
-const priority = document.querySelector('select');
-const deadline = document.querySelector('input[type="date"]');
-const submit = document.getElementById('submit');
+const table = document.querySelector('table');  //表
+const todo = document.getElementById('todo'); //todo
+const priority = document.querySelector('select'); //優先度
+const deadline = document.querySelector('input[type="date"]'); //日付(締め切り日）
+const submit = document.getElementById('submit'); //登録ボタン
 
-let list = [];
+//TODO リストの全データを「オブジェクトの配列」としてプログラムで持っておいて、それをストレージに書き出す処理
+let list = []; // TODOリストのデータ
 
 document.addEventListener('DOMContentLoaded', () => {
+  // 1. ストレージデータ（JSON）の読み込み
   const json = storage.todoList;
-  if (json == undefined) {
+  if (json == undefined) { // ストレージデータがない場合は何もしない
     return;
   }
-  list = JSON.parse(json);
+
+  // 2. JSONをオブジェクトの配列に変換して配列listに代入
+  list = JSON.parse(json); 
+  console.log(list);
+
+  // 3. 配列listのデータを元にテーブルに要素を追加
   for (const item of list) {
-    addItem(item);
+  addItem(item);
+  console.log(item);
   }
 });
 
+//item内にある情報を表示する処理を繰り返すための処理
 const addItem = (item) => {
-  const tr = document.createElement('tr');
+  const tr = document.createElement('tr'); // tr要素を生成
 
+  // オブジェクトの繰り返しはfor-in文、ループ定数（prop）のプロパティとしてitemを指定。
   for (const prop in item) {
-    const td = document.createElement('td');
-    if (prop == 'done') {
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.checked = item[prop];
-      td.appendChild(checkbox);
+    const td = document.createElement('td'); // td要素を生成
+
+    if (prop == 'done') { // 完了欄の場合の処理
+      const checkbox = document.createElement('input'); // 要素生成
+      checkbox.type = 'checkbox'; // type属性をcheckboxに
+      checkbox.checked = item[prop]; // checked属性を設定
+      td.appendChild(checkbox); // td要素の子要素に
       checkbox.addEventListener('change', checkBoxListener);
-    } else {
+    } else { //完了欄以外の場合の処理
       td.textContent = item[prop];
     }
     tr.appendChild(td);
@@ -49,43 +60,57 @@ const checkBoxListener = (ev) => {
   storage.todoList = JSON.stringify(list);
 };
 
+// 登録ボタンを押した際の処理。
 submit.addEventListener('click', () => {
-  const item = {};
+  const item = {}; //入力情報を一時保存するためのボックス
 
-  if (todo.value != '') {
+  // todoに情報が入っていない場合はダミーtodoと表示
+  if (todo.value != '') {todo.value
     item.todo = todo.value;
   } else {
     item.todo = 'ダミーTODO';
   }
+
+  //優先度
   item.priority = priority.value;
+
+  //日付（締め切り日）、入力がない場合は本日を入力
   if (deadline.value != '') {
     item.deadline = deadline.value;
   } else {
-    const date = new Date();
-    item.deadline = date.toLocaleDateString().replace(/\//g, '-');
+    window.alert('期日を入力してください');
+    return;
+    // 以下、警告を出すのではなく本日の日付を入れる形で代替する場合。
+      // const date = new Date(); // 本日の日付情報を取得
+      // item.deadline = date.toLocaleDateString().replace(/\//g, '-'); // /ではなく-へ置き換えて表示
   }
-  item.done = false;
 
+  item.done = false;
+  console.log(item); // 入力情報の確認
+
+  //フォーム情報のリセット
   todo.value = '';
-  priority.value = '普';
+  priority.value = '普通';
   deadline.value = '';
 
   addItem(item);
 
+  // ストレージに保存する前に、配列 list に最新 TODO データである item を追加（push）
   list.push(item);
   storage.todoList = JSON.stringify(list);
 });
 
+//絞り込み機能の処理
 const filterButton = document.createElement('button');
-filterButton.textContent = '優先度（高）で絞り込み';
+filterButton.textContent = '優先度（高い）で絞り込み';
 filterButton.id = 'priority';
 const main = document.querySelector('main');
 main.appendChild(filterButton);
 
 filterButton.addEventListener('click', () => {
-  clearTable();
+  clearTable(); //テーブル情報をクリアして、該当する優先順位のものに絞って表示する
   for (const item of list) {
-    if (item.priority == '高') {
+    if (item.priority == '高い') {
       addItem(item);
     }
   }
